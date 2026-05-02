4235c5da447d565febd895dcf56241455f5043b8
palak Batra <palakbatra79@gmail.com>
2026-05-02 10:19:31 +0530

    ok

--- Quiz_UI/src/pages/CreatorDashboard.jsx
+++ Quiz_UI/src/pages/CreatorDashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Plus, LogOut, Trash2, Users, Layout, Zap, Award } from "lucide-react";
+import { Plus, LogOut, Trash2, Users, Layout, Zap, Award, Shield } from "lucide-react";
 import API from "../utils/api";
 
 const CreatorDashboard = () => {
